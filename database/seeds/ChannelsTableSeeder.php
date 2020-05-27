bc8498d5b951deb0fa3acc762df2455c5a825013
Russkazkin <ruslan@skazkin.su>
2020-05-27 13:48:44 +0300

    Add chanels and users seeders

---
+++ database/seeds/ChannelsTableSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Channel;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+class ChannelsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Channel::create([
+            'name' => 'Laravel',
+            'slug' => Str::slug('Laravel'),
+        ]);
+
+        Channel::create([
+            'name' => 'Yii2',
+            'slug' => Str::slug('Yii2'),
+        ]);
+
+        Channel::create([
+            'name' => 'WordPress',
+            'slug' => Str::slug('WordPress'),
+        ]);
+
+        Channel::create([
+            'name' => 'Vue.js',
+            'slug' => Str::slug('Vue.js'),
+        ]);
+    }
+}
---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = User::where('email', 'ruslan@skazkin.su')->first();
+
+        if(!$user) {
+            User::create([
+                'name' => 'Rus Skazkin',
+                'email' => 'ruslan@skazkin.su',
+                'password' => Hash::make('master'),
+            ]);
+        }
+    }
+}
