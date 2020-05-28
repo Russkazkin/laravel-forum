01a16b1864f1d7b0293d6565c3e4d34b567458af
Russkazkin <ruslan@skazkin.su>
2020-05-28 14:06:39 +0300

    Add Add Discussion button

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
+
+    <div class="d-flex justify-content-end mb-2">
+        <a href="" class="btn btn-success">
+            Add Discussion
+        </a>
+    </div>
+
     <div class="card">
         <div class="card-header">Dashboard</div>
 
