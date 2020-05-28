8213c7a5edd9f851815f4783ac3b995b63e4e45b
Russkazkin <ruslan@skazkin.su>
2020-05-28 14:42:56 +0300

    Add create discussion form

--- resources/views/discussions/create.blade.php
+++ resources/views/discussions/create.blade.php
@@ -6,7 +6,26 @@
         <div class="card-header">Add Discussion</div>
 
         <div class="card-body">
-
+            <form action="{{ route('discussion.store') }}">
+                @csrf
+                <div class="form-group">
+                    <label for="title">Title</label>
+                    <input type="text" class="form-control" name="title" id="title">
+                </div>
+                <div class="form-group">
+                    <label for="content">Content</label>
+                    <textarea class="form-control" name="content" id="content" cols="5" rows="5"></textarea>
+                </div>
+                <div class="form-group">
+                    <label for="channel">Channel</label>
+                    <select name="channel" id="channel" class="form-control">
+                        @foreach($channels as $channel)
+                            <option value="{{ $channel->id }}">{{ $channel->name }}</option>
+                        @endforeach
+                    </select>
+                </div>
+                <button type="submit" class="btn btn-success">Create Discussion</button>
+            </form>
         </div>
     </div>
 @endsection
