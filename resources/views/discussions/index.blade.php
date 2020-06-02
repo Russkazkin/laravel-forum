b731748101163881a90f595fda28738ee5350ba0
Russkazkin <ruslan@skazkin.su>
2020-06-02 14:04:03 +0300

    Add laravel-gravatar package

--- resources/views/discussions/index.blade.php
+++ resources/views/discussions/index.blade.php
@@ -3,7 +3,9 @@
 @section('content')
     @foreach($discussions as $discussion)
     <div class="card mb-3">
-        <div class="card-header">{{ $discussion->title }}</div>
+        <div class="card-header">
+            <img src="" alt="">
+        </div>
 
         <div class="card-body">
            {!! $discussion->content !!}
