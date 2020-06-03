c78e4a8807659c7f0612c1785b5f54553b0ec2c7
Russkazkin <ruslan@skazkin.su>
2020-06-03 13:40:09 +0300

    Add relationship methods to Reply model

--- app/Reply.php
+++ app/Reply.php
@@ -2,9 +2,15 @@
 
 namespace LaravelForum;
 
-use Illuminate\Database\Eloquent\Model;
-
 class Reply extends Model
 {
-    //
+    public function owner()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function discussion()
+    {
+        return $this->belongsTo(Discussion::class);
+    }
 }
