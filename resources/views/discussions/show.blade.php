90dc98e74a649bd6e2a5bc2880daeb91078a22c0
Russkazkin <ruslan@skazkin.su>
2020-06-03 13:02:25 +0300

    Add add reply form

--- resources/views/discussions/show.blade.php
+++ resources/views/discussions/show.blade.php
@@ -13,4 +13,24 @@
             {!! $discussion->content !!}
         </div>
     </div>
+    <div class="card my-5">
+        <div class="card-header">
+            Add a reply
+        </div>
+        <div class="card-body">
+            <form action="" method="POST">
+                @csrf
+                <input type="hidden" name="reply" id="reply">
+                <trix-editor input="reply"></trix-editor>
+            </form>
+        </div>
+    </div>
+@endsection
+
+@section('css')
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/trix/1.2.3/trix.min.css">
+@endsection
+
+@section('js')
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/trix/1.2.3/trix.min.js"></script>
 @endsection
