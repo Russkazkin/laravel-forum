4206710dff9bfc10ebd3aec9b5d1ab896acdcd45
Russkazkin <ruslan@skazkin.su>
2020-06-04 09:20:01 +0300

    Complete replies migration

--- database/migrations/2020_06_03_101308_create_replies_table.php
+++ database/migrations/2020_06_03_101308_create_replies_table.php
@@ -15,7 +15,7 @@ class CreateRepliesTable extends Migration
     {
         Schema::create('replies', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->onDelete('set null')->onUpdate('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('restrict')->onUpdate('cascade');
             $table->foreignId('discussion_id')->constrained()->onUpdate('cascade')->onDelete('cascade');
             $table->text('content');
             $table->timestamps();
@@ -29,6 +29,10 @@ class CreateRepliesTable extends Migration
      */
     public function down()
     {
+        Schema::table('replies', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
+            $table->dropForeign(['discussion_id']);
+        });
         Schema::dropIfExists('replies');
     }
 }
