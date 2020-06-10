c1a9cfe4adafa7676236bfe8df792fda6e1cddab
Russkazkin <ruslan@skazkin.su>
2020-06-10 13:26:04 +0300

    Add channels links to the sidebar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -109,8 +109,11 @@
                         <div class="card-body">
                             <ul class="list-group">
                                 @foreach($channels as $channel)
+
                                     <li class="list-group-item">
-                                        {{ $channel->name }}
+                                        <a href="{{ route('discussions.index') }}?channel={{ $channel->slug }}">
+                                            {{ $channel->name }}
+                                        </a>
                                     </li>
                                 @endforeach
                             </ul>
