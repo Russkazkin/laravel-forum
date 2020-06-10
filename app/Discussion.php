6d1b60e37e38ea31281f558bb54c812e740a1464
Russkazkin <ruslan@skazkin.su>
2020-06-10 14:54:24 +0300

    Disable self notify

--- app/Discussion.php
+++ app/Discussion.php
@@ -57,6 +57,10 @@ class Discussion extends Model
             'reply_id' => $reply->id,
         ]);
 
+        if($reply->owner->id == $this->author->id) {
+            return;
+        }
+
         $reply->owner->notify(new ReplyMarkedAsBestReply($reply->discussion));
     }
 
